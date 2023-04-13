30108d63af2d27cf6111f10d34aadb9a855b66f9
José Ferreira <up202108836@g.uporto.pt>
2023-04-13 18:44:27 +0100

    1 iteration database

--- database/tables.sql
+++ database/tables.sql
@@ -5,18 +5,28 @@ CREATE TABLE Users (
     email VARCHAR(50) NOT NULL,
     full_name VARCHAR(100) NOT NULL,
     created_at DATETIME NOT NULL,
-    isAdmin INTEGER NOT NULL CHECK (isAdmin IN (0,1)),
-    isAgent INTEGER NOT NULL CHECK (isAgent IN (0,1))
+    
 );
 
 CREATE TABLE Tickets (
     ticket_id INTEGER PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL, --user que criou ticket
     title VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
     status VARCHAR(20) NOT NULL,
     priority VARCHAR(20) NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    isClosed INTEGER NOT NULL CHECK (isClosed IN (0,1)),
+    user_assigned_id INTEGER ,
+    --departamento
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (user_assigned_id) REFERENCES Users(user_id)
+
+    --departamento
+    
+);
+
+CREATE TABLE Mensagem (
+
 );
